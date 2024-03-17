88e5b54fc6ecf9a43bd754c3c487eb2cd9c0fb19
Madusanka Madiligama <83027416+mbabeykoon@users.noreply.github.com>
2024-03-17 15:34:27 -0500

    Your commit message

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# Text Summarizer Project
+# End to end Text Summarizer Project
+
+## Workflows
+1. Update the config.yaml
+2. Update the params.yaml
+3. Update the entity
+4. Update the configuration manager in src config
+5. Update the conponents
+6. Update the pipeline
+7. Update the main.py
+8. Update the app.py
--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 from textSummarizer.logging import logger
+from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("welcome to our coustom logging")
+STAGE_NAME = "Data Ingestion stage"
+try:
+    logger.info(f">>>>>>>> stage {STAGE_NAME} started <<<<<<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>>>> stage {STAGE_NAME} completed <<<<<<<<<")
+
+except Exception as e:
+    logger.exception(e)
+    raise e
